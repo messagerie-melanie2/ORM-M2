dc564840c5f28bf0f691855a767560dd4ec55d20
Thomas Payen <thomas.payen@i-carre.net>
2017-04-21 10:34:44 +0200

    Ajout du includes_conf dans le composer

--- Version.php
+++ Version.php
@@ -32,16 +32,16 @@ class Version {
     /**
      * Numéro de version
      */
-    const VERSION = '0.3.0.2';
+    const VERSION = '0.3.0.3';
     
     /**
      * Numéro de version normalisé
      */
-    const NORMALIZED_VERSION = '0.3.0.2';
+    const NORMALIZED_VERSION = '0.3.0.3';
     
     /**
      * Build
      */
-    const BUILD = '20170420181452';
+    const BUILD = '20170421103452';
 
 }

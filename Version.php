d6e86345505ce7a92dd99f7494d1afbd9f07cb9f
Thomas Payen <thomas.payen@i-carre.net>
2017-04-21 11:08:42 +0200

    Inversion files dans le composer

--- Version.php
+++ Version.php
@@ -32,16 +32,16 @@ class Version {
     /**
      * Numéro de version
      */
-    const VERSION = '0.3.0.3';
+    const VERSION = '0.3.0.4';
     
     /**
      * Numéro de version normalisé
      */
-    const NORMALIZED_VERSION = '0.3.0.3';
+    const NORMALIZED_VERSION = '0.3.0.4';
     
     /**
      * Build
      */
-    const BUILD = '20170421103452';
+    const BUILD = '20170421110652';
 
 }

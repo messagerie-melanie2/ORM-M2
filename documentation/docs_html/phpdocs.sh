a33f70d677ae4158ae6c168d6f719cd0541e9e3d
Thomas Payen <thomas.payen@i-carre.net>
2018-02-27 16:25:19 +0100

    Ajout des docs générés par ApiGen

--- documentation/docs_html/phpdocs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-phpdoc --title="Lib ORM Mélanie2" --target="documentation/docs_html" --directory="api,cache,exceptions,interfaces,ldap,lib,log,objects,sql" --ignore="lib/iCalcreator.class.php"

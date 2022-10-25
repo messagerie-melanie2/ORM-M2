85d8e65c9543f8c8ae2ec97a6d5cab0c51009911
samir.keriou <samir.keriou@gendarmerie.interieur.gouv.fr>
2022-10-25 15:55:50 +0200

    test resource object

--- src/Api/Gn/Group.php
+++ src/Api/Gn/Group.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Ce fichier est développé pour la gestion de la lib MCE
- * 
+ *
  * Cette Librairie permet d'accèder aux données sans avoir à implémenter de couche SQL
  * Des objets génériques vont permettre d'accèder et de mettre à jour les données
- * 
+ *
  * ORM Mél Copyright © 2021 Groupe Messagerie/MTE
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -26,13 +26,13 @@ use LibMelanie\Config\Ldap as LdapConfig;
 
 /**
  * Classe groupe LDAP pour GN
- * 
+ *
  * @author Groupe Messagerie/MTE - Apitech
  * @package LibMCE
  * @subpackage API/GN
  * @api
- * 
- * @property string $dn DN du groupe l'annuaire 
+ *
+ * @property string $dn DN du groupe l'annuaire
  * @property string $fullname Nom complet du groupe LDAP
  * @property string $type Type de groupe (voir Mce\Users\Type::*)
  * @property string $email Adresse email principale de l'utilisateur
@@ -49,7 +49,7 @@ use LibMelanie\Config\Ldap as LdapConfig;
 class Group extends Defaut\Group {
     /**
      * Attributs par défauts pour la méthode load()
-     * 
+     *
      * @ignore
      */
     const LOAD_ATTRIBUTES = ['dn', 'fullname', 'email', 'owners', 'cn'];
@@ -57,7 +57,7 @@ class Group extends Defaut\Group {
     /**
      * Récupère la liste des membres d'un groupe
      * On ne "load" pas les membres, car certains groupes sont trop grand => trop de requêtes
-     * 
+     *
      * @return array|Defaut\User[]
      */
     public function getMapMembers() {
@@ -69,7 +69,7 @@ class Group extends Defaut\Group {
             $filter = str_replace("%%memberOf%%", $this->dn, $filter);
             $search = $ldap->search($ldap->getConfig("base_dn"), $filter);
             $entries = $ldap->get_entries($search);
-
+print_r($entries);die;
             $attributesMember = $ldap->getConfig('get_user_infos_attributes');
 
             if ($entries

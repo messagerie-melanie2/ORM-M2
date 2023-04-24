e63de0be9cd34ffa85cc426719311598a8650df4
KERIOU Samir <samir.keriou@gendarmerie.interieur.gouv.fr>
2023-04-24 16:02:10 +0200

    modif conf new ldap schema

--- src/Api/Gn/User.php
+++ src/Api/Gn/User.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Ce fichier est développé pour la gestion de la lib MCE
- * 
+ *
  * Cette Librairie permet d'accèder aux données sans avoir à implémenter de couche SQL
  * Des objets génériques vont permettre d'accèder et de mettre à jour les données
- * 
+ *
  * ORM Mél Copyright © 2021 Groupe Messagerie/MTE
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -29,12 +29,12 @@ use LibMelanie\Config\MappingMce;
 /**
  * Classe utilisateur pour GN
  * basé sur le User MCE
- * 
+ *
  * @author Groupe Messagerie/MTE - Apitech
  * @package LibMCE
  * @subpackage API/GN
  * @api
- * 
+ *
  * @property string $dn DN de l'utilisateur dans l'annuaire
  * @property string $uid Identifiant unique de l'utilisateur
  * @property string $fullname Nom complet de l'utilisateur
@@ -53,13 +53,13 @@ use LibMelanie\Config\MappingMce;
  * @property array $server_routage Champ de routage pour le serveur de message de l'utilisateur
  * @property-read string $server_host Host du serveur de messagerie de l'utilisateur
  * @property-read string $server_user User du serveur de messagerie de l'utilisateur
- * 
+ *
  * @property-read boolean $is_objectshare Est-ce que cet utilisateur est en fait un objet de partage
  * @property-read ObjectShare $objectshare Retourne l'objet de partage lié à cet utilisateur si s'en est un
- * 
+ *
  * @property-read boolean $is_synchronisation_enable Est-ce que la synchronisation est activée pour l'utilisateur ?
  * @property-read string $synchronisation_profile Profil de synchronisation positionné pour l'utilisateur (STANDARD ou SENSIBLE)
- * 
+ *
  * @method string getTimezone() [OSOLETE] Chargement du timezone de l'utilisateur
  * @method bool authentification($password, $master = false) Authentification de l'utilisateur sur l'annuaire Mélanie2
  * @method bool save() Enregistrement de l'utilisateur dans l'annuaire
@@ -69,10 +69,10 @@ use LibMelanie\Config\MappingMce;
 class User extends Mce\User {
   /**
    * Attributs par défauts pour la méthode load()
-   * 
+   *
    * @ignore
    */
-  const LOAD_ATTRIBUTES = ['fullname', 'uid', 'name', 'email', 'email_list', 'email_send', 'email_send_list', 'server_routage', 'shares', 'type','mcemailroutingaddress','outofoffices'];
+  const LOAD_ATTRIBUTES = ['fullname', 'uid', 'name', 'email', 'email_list', 'email_send', 'email_send_list', 'server_routage', 'shares', 'type','mailroutingaddress','outofoffices'];
 
 	/**
    * Configuration du mapping qui surcharge la conf
@@ -95,12 +95,11 @@ class User extends Mce\User {
     "postalcode"              => 'postalcode',                    // Code postal
     "locality"                => 'l',                             // Ville
     "title"                   => 'title',                         // Titre
-    "memberof"                => [MappingMce::name => 'memberof', MappingMce::type => MappingMce::arrayLdap],
+    "gendmemberof"                => [MappingMce::name => 'gendmemberof', MappingMce::type => MappingMce::arrayLdap],
     "outofoffices"            => [MappingMce::name => 'mcevacation', MappingMce::type => MappingMce::arrayLdap], // Affichage du message d'absence de l'utilisateur
-    "mcemailroutingaddress"   => [MappingMce::name => 'mcemailroutingaddress', MappingMce::type => MappingMce::arrayLdap], // routegemceadrressmail host
+    "mailroutingaddress"   => [MappingMce::name => 'mailroutingaddress', MappingMce::type => MappingMce::arrayLdap], // routegemceadrressmail host
     "deliverymode"            => [MappingMce::name => 'deliverymode', MappingMce::type => MappingMce::stringLdap],
-    "codeunite"               => 'codeunite',
-    "displayname"             => 'displayname', 
+    "displayname"             => 'displayname',
     "employeenumber"          => 'employeenumber',
     "givenname"               => 'givenname',
   ];
@@ -154,7 +153,7 @@ class User extends Mce\User {
 
   /**
    * Mapping shares field
-   * 
+   *
    * @return Share[] Liste des partages positionnés sur cette boite
    */
   protected function getMapShares() {
@@ -174,7 +173,7 @@ class User extends Mce\User {
 
   /**
    * Mapping shares field
-   * 
+   *
    * @return array Liste des partages supportés par cette boite ([Share::TYPE_*])
    */
   protected function getMapSupported_shares() {
@@ -183,7 +182,7 @@ class User extends Mce\User {
 
   /**
    * Récupération du champ out of offices
-   * 
+   *
    * @return Outofoffice[] Tableau de d'objets Outofoffice
    */
   protected function getMapOutofoffices() {
@@ -207,7 +206,7 @@ class User extends Mce\User {
 
   /**
    * Positionnement du champ out of offices
-   * 
+   *
    * @param Outofoffice[] $OofObjects
    */
   protected function setMapOutofoffices($OofObjects) {

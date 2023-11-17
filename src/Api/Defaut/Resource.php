73db832d3aac41f8831928a99e0414457f264e6e
samir.keriou <samir.keriou@gendarmerie.interieur.gouv.fr>
2023-11-17 15:48:40 +0100

    comment resource

--- src/Api/Defaut/Resource.php
+++ src/Api/Defaut/Resource.php
@@ -5,6 +5,18 @@ namespace LibMelanie\Api\Defaut;
 use LibMelanie\Lib\MceObject;
 use LibMelanie\Objects\ResourceMelanie;
 
+/**
+ * Classe resource par defaut
+ * Certains champs sont mappés directement
+ *
+ * @author Groupe Messagerie/MTE - Apitech
+ * @package LibMCE
+ * @subpackage API/Defaut
+ * @api
+ * @method bool exists() Test si le partage existe, en fonction de l'object_id et du nom
+ * @method bool save() Sauvegarde la priopriété dans la base de données
+ * @method bool delete() Supprime le partage, en fonction de l'object_id et du nom
+ */
 class Resource extends MceObject
 {
     public function __construct(int $id = null)
